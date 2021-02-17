6801ca6dcc7f5a6e642ae2c5a648778cdf9cfa93
nebulaOW <religiossdoge@gmail.com>
2021-02-16 22:03:23 -0600

    Delete hidden message upon deleted PB original post

--- cogs/verification.py
+++ cogs/verification.py
@@ -50,6 +50,10 @@ class Verification(commands.Cog, name="Verification"):
         if payload is not None:
             search = await WorldRecords.find_one({"message_id": payload.message_id})
             if search is not None:
+                channel = self.bot.get_channel(constants.HIDDEN_VERIFICATION_CHANNEL)
+                hidden_msg = await channel.fetch_message(search.hidden_id)
+                if hidden_msg:
+                    await hidden_msg.delete()
                 await search.delete()
 
 
